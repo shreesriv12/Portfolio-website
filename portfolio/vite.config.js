cb76947fa6e858e0b7a898ee2011f4e0f5cf4e7e
shreesriv12 <shreeyasrivastava1124@gmail.com>
2025-03-18 00:06:46 +0530

    set up

---
+++ portfolio/vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        generalsans: ['General Sans', 'sans-serif'],
+      },
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: '#010103',
+          200: '#0E0E10',
+          300: '#1C1C21',
+          500: '#3A3A49',
+          600: '#1A1A1A',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          800: '#E4E4E6',
+          700: '#D6D9E9',
+          600: '#AFB0B6',
+          500: '#62646C',
+        },
+      },
+      backgroundImage: {
+        terminal: "url('/assets/terminal.png')",
+      },
+    },
+  },
+  plugins: [react(),
+    tailwindcss(),
+
+  ],
+})
